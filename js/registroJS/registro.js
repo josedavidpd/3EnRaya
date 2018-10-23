5c98c66a95840ebac0f9ef39957f58bed682102b
JoseDavid <josedavid97.pd@gmail.com>
2018-10-23 22:40:06 +0200

    Corregido fallo css registro y empezando ajax registro

--- js/registroJS/registro.js
+++ js/registroJS/registro.js
@@ -1,38 +1,35 @@
-/*$(document).ready(function () {
+$(document).ready(function () {
 
-    const regexSoloLetras = /^[a-zA-Z]+$/;
+    $("#btn-registro").on('click', function (e) {
+        e.preventDefault();
+        var em = $('#email').val();
+        var pwd = $('#password').val();
 
-    
-
-    $("#formRegistro").on('submit', (e) => {
-        
-        if (regexSoloLetras.test($("#nombreRegistro").val().trim())) {
-            $("#errorNombre").addClass("d-none");
-            return true;
-        } else {
-            $("#errorNombre").removeClass("d-none");
-            return false;
-
-   
-    
-
-    }});
-
-    $("#formRegistro").on('submit', (e) => {
-
-        if (regexSoloLetras.test($("#apellidosRegistro").val().trim())) {
-            $("#errorApellidos").addClass("d-none");
-            return true;
-        } else {
-            $("#errorApellidos").removeClass("d-none");
-            return false;
+        $.ajax({
+            method: "POST",
+            url:
+                "http://www.miguelcamposrivera.com:3008/tictactoeapi/auth/login",
+            dataType: "json",
+            data: { email: em, password: pwd }
+        })
+            .done(function (user) {
+                localStorage.setItem("token", user.token);
+                localStorage.setItem("username", user.username);
+                localStorage.setItem("email", user.email);
 
 
+                location.replace('../../html/ranking.html');
+            })
+            .fail(function (resp) {
+               $("#errorLogin").removeClass('d-none');
+                console.log("ERROR RESPUESTA");
+                console.log(resp);
+            });
 
-
-        }
     });
+
+    
     
 
     
-});*/
+});
